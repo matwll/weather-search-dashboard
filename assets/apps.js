c155c6806d8537bf08b4ccb32fd3d66b3a2b1576
Matthew Williams <maw01091989@gmail.com>
2022-11-06 13:14:21 -0800

    adjusted some css elements, and added the creation of the 5 day forcast cards

--- assets/apps.js
+++ assets/apps.js
@@ -1,7 +1,17 @@
 var citySearch = document.querySelector("#city-search").value;
 var searchBtn = document.querySelector(".search");
-var liContainer = document.querySelector('.list-group');
-var cardTitle = document.querySelector('.card-body');
+var liContainer1 = document.querySelector('.list-group1');
+var liContainer2 = document.querySelector('.list-group2');
+var liContainer3 = document.querySelector('.list-group3');
+var liContainer4 = document.querySelector('.list-group4');
+var liContainer5 = document.querySelector('.list-group5');
+var liContainer6 = document.querySelector('.list-group6');
+var cardTitle1 = document.querySelector('.card-body1');
+var cardTitle2 = document.querySelector('.card-body2');
+var cardTitle3 = document.querySelector('.card-body3');
+var cardTitle4 = document.querySelector('.card-body4');
+var cardTitle5 = document.querySelector('.card-body5');
+var cardTitle6 = document.querySelector('.card-body6');
 
 var lat;
 var lon;
@@ -19,6 +29,7 @@ function searchLatLon() {
       return response.json();
     })
     .then(function (data) {
+      console.log(data);
       //get the latitude and longitude from the city search to perform another api call with
       lat = data[0].lat;
       lon = data[0].lon;
@@ -35,6 +46,7 @@ function searchLatLon() {
           return response.json();
         })
         .then(function (data) {
+          // arrary values needed = [0,7,15,23,31,39]
           //store the data variables to display for user
           var date = data.list[0].dt;
           var icon = data.list[0].weather[0].icon;
@@ -45,28 +57,147 @@ function searchLatLon() {
           date = (date * 1000);
           var newDate = new Date(date)
           date = newDate.toLocaleDateString();
-          console.log(date, icon, temp, windSpeed, humidity);
-
+          //create elements to display the information and append to the document
           var titleEl = document.createElement('h5');
           titleEl.classList.add('card-title');
           titleEl.innerText = (citySearch + "(" + date + ")");
-          cardTitle.appendChild(titleEl);
+          cardTitle1.appendChild(titleEl);
           var iconImg = document.createElement('img');
           iconImg.src = "http://openweathermap.org/img/w/" + icon + ".png";
-          cardTitle.appendChild(iconImg);
+          cardTitle1.appendChild(iconImg);
 
           weatherArr = [temp, windSpeed, humidity]
           for (let i = 0; i < weatherArr.length; i++){
             var liEl = document.createElement('li');
             liEl.classList.add('list-group-item');
             liEl.innerText = weatherArr[i];
-            liContainer.appendChild(liEl);
+            liContainer1.appendChild(liEl);
+          }
+          var date = data.list[7].dt;
+          var icon = data.list[7].weather[0].icon;
+          var temp = data.list[7].main.temp;
+          var windSpeed = data.list[7].wind.speed;
+          var humidity = data.list[7].main.humidity;
+          //convert the unix timestamp into an understandable date, and add it to the Card
+          date = (date * 1000);
+          var newDate = new Date(date)
+          date = newDate.toLocaleDateString();
+          //create elements to display the information and append to the document
+          var titleEl = document.createElement('h5');
+          titleEl.classList.add('card-title');
+          titleEl.innerText = (citySearch + "(" + date + ")");
+          cardTitle2.appendChild(titleEl);
+          var iconImg = document.createElement('img');
+          iconImg.src = "http://openweathermap.org/img/w/" + icon + ".png";
+          cardTitle2.appendChild(iconImg);
+
+          weatherArr = [temp, windSpeed, humidity]
+          for (let i = 0; i < weatherArr.length; i++){
+            var liEl = document.createElement('li');
+            liEl.classList.add('list-group-item');
+            liEl.innerText = weatherArr[i];
+            liContainer2.appendChild(liEl);
+          }
+          var date = data.list[15].dt;
+          var icon = data.list[15].weather[0].icon;
+          var temp = data.list[15].main.temp;
+          var windSpeed = data.list[15].wind.speed;
+          var humidity = data.list[15].main.humidity;
+          //convert the unix timestamp into an understandable date, and add it to the Card
+          date = (date * 1000);
+          var newDate = new Date(date)
+          date = newDate.toLocaleDateString();
+          //create elements to display the information and append to the document
+          var titleEl = document.createElement('h5');
+          titleEl.classList.add('card-title');
+          titleEl.innerText = (citySearch + "(" + date + ")");
+          cardTitle3.appendChild(titleEl);
+          var iconImg = document.createElement('img');
+          iconImg.src = "http://openweathermap.org/img/w/" + icon + ".png";
+          cardTitle3.appendChild(iconImg);
+
+          weatherArr = [temp, windSpeed, humidity]
+          for (let i = 0; i < weatherArr.length; i++){
+            var liEl = document.createElement('li');
+            liEl.classList.add('list-group-item');
+            liEl.innerText = weatherArr[i];
+            liContainer3.appendChild(liEl);
+          }
+          var date = data.list[23].dt;
+          var icon = data.list[23].weather[0].icon;
+          var temp = data.list[23].main.temp;
+          var windSpeed = data.list[23].wind.speed;
+          var humidity = data.list[23].main.humidity;
+          //convert the unix timestamp into an understandable date, and add it to the Card
+          date = (date * 1000);
+          var newDate = new Date(date)
+          date = newDate.toLocaleDateString();
+          //create elements to display the information and append to the document
+          var titleEl = document.createElement('h5');
+          titleEl.classList.add('card-title');
+          titleEl.innerText = (citySearch + "(" + date + ")");
+          cardTitle4.appendChild(titleEl);
+          var iconImg = document.createElement('img');
+          iconImg.src = "http://openweathermap.org/img/w/" + icon + ".png";
+          cardTitle4.appendChild(iconImg);
+
+          weatherArr = [temp, windSpeed, humidity]
+          for (let i = 0; i < weatherArr.length; i++){
+            var liEl = document.createElement('li');
+            liEl.classList.add('list-group-item');
+            liEl.innerText = weatherArr[i];
+            liContainer4.appendChild(liEl);
+          }
+          var date = data.list[31].dt;
+          var icon = data.list[31].weather[0].icon;
+          var temp = data.list[31].main.temp;
+          var windSpeed = data.list[31].wind.speed;
+          var humidity = data.list[31].main.humidity;
+          //convert the unix timestamp into an understandable date, and add it to the Card
+          date = (date * 1000);
+          var newDate = new Date(date)
+          date = newDate.toLocaleDateString();
+          //create elements to display the information and append to the document
+          var titleEl = document.createElement('h5');
+          titleEl.classList.add('card-title');
+          titleEl.innerText = (citySearch + "(" + date + ")");
+          cardTitle5.appendChild(titleEl);
+          var iconImg = document.createElement('img');
+          iconImg.src = "http://openweathermap.org/img/w/" + icon + ".png";
+          cardTitle5.appendChild(iconImg);
+
+          weatherArr = [temp, windSpeed, humidity]
+          for (let i = 0; i < weatherArr.length; i++){
+            var liEl = document.createElement('li');
+            liEl.classList.add('list-group-item');
+            liEl.innerText = weatherArr[i];
+            liContainer5.appendChild(liEl);
+          }
+          var date = data.list[39].dt;
+          var icon = data.list[39].weather[0].icon;
+          var temp = data.list[39].main.temp;
+          var windSpeed = data.list[39].wind.speed;
+          var humidity = data.list[39].main.humidity;
+          //convert the unix timestamp into an understandable date, and add it to the Card
+          date = (date * 1000);
+          var newDate = new Date(date)
+          date = newDate.toLocaleDateString();
+          //create elements to display the information and append to the document
+          var titleEl = document.createElement('h5');
+          titleEl.classList.add('card-title');
+          titleEl.innerText = (citySearch + "(" + date + ")");
+          cardTitle6.appendChild(titleEl);
+          var iconImg = document.createElement('img');
+          iconImg.src = "http://openweathermap.org/img/w/" + icon + ".png";
+          cardTitle6.appendChild(iconImg);
+
+          weatherArr = [temp, windSpeed, humidity]
+          for (let i = 0; i < weatherArr.length; i++){
+            var liEl = document.createElement('li');
+            liEl.classList.add('list-group-item');
+            liEl.innerText = weatherArr[i];
+            liContainer6.appendChild(liEl);
           }
         });
     });
-  citySearch;
 }
-
-
-
-// arrary values needed = [0,7,15,23,31,39]
